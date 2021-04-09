fa85dac715eb9539607f478efd739713b70bb5f6
Christian Marquez <christianmarquez41@gmail.com>
2021-04-09 19:27:47 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import NavbarComponent from "./components/Navbar"
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <NavbarComponent />
     </div>
   );
 }
