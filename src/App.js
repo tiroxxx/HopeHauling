27fb407643a5ffaf7e2d9a0a08cc100f38c19050
Christian Marquez <christianmarquez41@gmail.com>
2021-04-09 19:41:25 -0400

    added landing picture

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavbarComponent from "./components/Navbar"
+import TractorDumpster from "./images/tractor-dumpster.jpg"
+import { Image } from 'react-bootstrap';
 
 function App() {
   return (
     <div className="App">
       <NavbarComponent />
+      <Image src={TractorDumpster} fluid />
     </div>
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,12 +4,13 @@ import { Nav, Navbar } from "react-bootstrap"
 export default function NavbarComponent() {
     return (
         <>
-            <Navbar bg="dark" variant="dark">
-                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-                <Nav className="mr-auto">
-                    <Nav.Link href="#home">Home</Nav.Link>
+            <Navbar className="justify-content-between" bg="dark" variant="dark">
+                <Navbar.Brand href="#home">Hope Hauling</Navbar.Brand>
+                <Nav>
                     <Nav.Link href="#features">Features</Nav.Link>
                     <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    <Nav.Link href="#AboutUs">About Us</Nav.Link>
+                    <Nav.Link href="#Contact">123-456-7890</Nav.Link>
                 </Nav>
             </Navbar>
         </>
